fa7340b433c1767ebb966830b721da9c9e3df37a
Iuri-crypto <lorenzop0o9i8u7@gmail.com>
2025-06-12 17:01:49 -0400

    Apagando

--- modela sistema eletrico/bdgd_to_opendss/validation/config_loader.py
+++
@@ -1,9 +0,0 @@
-
-import yaml
-import os
-
-def load_validation_config(section: str):
-    config_path = os.path.join("bdgd_to_opendss", "validation", "validation_config.yaml")
-    with open(config_path, "r") as file:
-        config = yaml.safe_load(file)
-        return config.get(section, {})  
